cdbea5179d17cde23932816e62b24397794ae36f
Mohit Kumar Singh <mohit.kumar@nickelfox.co>
2022-03-03 10:19:41 +0530

    initial commit

---
+++ IFSTAModule/ExamPrep/TestClassVamp.swift
@@ -0,0 +1,15 @@
+//
+//  TestClassVamp.swift
+//  IFSTAModule
+//
+//  Created by Mohit Kumar Singh on 03/03/22.
+//
+
+import Foundation
+
+public class TestClassVamp {
+    
+    static func printLog() {
+        print("Stable Framework")
+    }
+}
