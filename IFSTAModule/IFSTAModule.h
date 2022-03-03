d8ae02c5e529b791fa6d098f16f99659b2437693
Mohit Kumar Singh <mohit.kumar@nickelfox.co>
2022-03-03 10:14:10 +0530

    Initial Commit

---
+++ IFSTAModule/IFSTAModule.h
@@ -0,0 +1,18 @@
+//
+//  IFSTAModule.h
+//  IFSTAModule
+//
+//  Created by Mohit Kumar Singh on 03/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IFSTAModule.
+FOUNDATION_EXPORT double IFSTAModuleVersionNumber;
+
+//! Project version string for IFSTAModule.
+FOUNDATION_EXPORT const unsigned char IFSTAModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IFSTAModule/PublicHeader.h>
+
+
